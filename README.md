4ba5d83c1c30223b202448b4b9487e0b49ba9bfc
Polina Panayotova <pjatanasova@yahoo.com>
2022-08-06 12:18:33 +0100

    initial project structure created

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # -B8IT150
+
+## Requirements
+
+* PHP 8.1
+* Composer
+
+## How to run the project
+
+1. Run 'composer install'
+2. Run 'composer start'
---
+++ html/controllers/HomeController.php
@@ -0,0 +1,7 @@
+<?php
+
+final class HomeController {
+    function loadINdexPage(\Base $f3, array $args): void {
+        echo 'Test home page';
+    }
+}
