59d385351bb1a40ea7d77fe9fea4870e242d3e0e
Polina Panayotova <pjatanasova@yahoo.com>
2022-08-28 11:31:48 +0100

    satrted actors screen

--- html/controllers/ActorsController.php
+++ html/controllers/ActorsController.php
@@ -1,7 +1,27 @@
 <?php
 
+use Tmdb\Client;
+use Tmdb\Repository\PeopleRepository;
+use Tmdb\Factory\ImageFactory;
+use Tmdb\Helper\ImageHelper;
+use Tmdb\Model\Image;
+use Tmdb\Repository\ConfigurationRepository;
+
 final class ActorsController {
     function get(Base $f3, array $args): void {
+
+        $client = ApiClient::makeClient();
+        $repository = new PeopleRepository($client);
+        $people = $repository->getPopular();
+        $data = [];
+        foreach($people as $person) {
+            // var_dump($person);
+            array_push($data, [
+                'name' => $person->getName(),
+                // 'dateOfBirth'=>$person->getBirthday()->format('l d F Y')
+            ]);
+        }
+        $f3->set('list', $data);
         $f3->set('pageTitle', 'Actors');
         $f3->set('content', 'actors.htm');
         echo \Template::instance()->render('main.htm');
