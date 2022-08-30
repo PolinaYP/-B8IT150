65870afbecc43e98408468c7e7c3ec643e57d2b2
Polina Panayotova <polly@Panayots-Mac-mini.local>
2022-08-30 20:10:48 +0100

    added actors functionality

--- html/controllers/ActorsController.php
+++ html/controllers/ActorsController.php
@@ -2,6 +2,8 @@
 
 use Tmdb\Client;
 use Tmdb\Repository\PeopleRepository;
+use Tmdb\Repository\SearchRepository;
+use Tmdb\Model\Search\SearchQuery\PersonSearchQuery;
 use Tmdb\Factory\ImageFactory;
 use Tmdb\Helper\ImageHelper;
 use Tmdb\Model\Image;
@@ -9,21 +11,45 @@ use Tmdb\Repository\ConfigurationRepository;
 
 final class ActorsController {
     function get(Base $f3, array $args): void {
-
         $client = ApiClient::makeClient();
-        $repository = new PeopleRepository($client);
-        $people = $repository->getPopular();
+        $searchString = $f3->GET['search'] ?? NULL;
+        $people = [];
+        if(isset($searchString)) {
+            $query = new PersonSearchQuery();
+            $query->page(1);
+            $repository = new SearchRepository($client);
+            $people = $repository->searchPerson($searchString, $query);['results'];
+            $f3->set('searchString', $searchString);
+        } else {
+            $repository = new PeopleRepository($client);
+            $people = $repository->getPopular();
+        }
+        
         $data = [];
         foreach($people as $person) {
-            // var_dump($person);
             array_push($data, [
                 'name' => $person->getName(),
-                // 'dateOfBirth'=>$person->getBirthday()->format('l d F Y')
+                'image' => $person->getProfilePath() !== null ? 'http://image.tmdb.org/t/p/w300' . $person->getProfilePath() : '../images/No_image_available.jpeg',
+                'id' => $person->getId()
             ]);
         }
-        $f3->set('list', $data);
+        $f3->set('people', $data);
         $f3->set('pageTitle', 'Actors');
         $f3->set('content', 'actors.htm');
+        echo \Template::instance()->render('main.htm');
+    }
+
+    public function getActorDetails(Base $f3, array $args): void {
+        $client = ApiClient::makeClient();
+        $repository = new PeopleRepository($client);
+        $actor = $repository->load($args['actorId']);
+        $f3->set('BREADCRUMB', [
+            ['title' => 'Actors', 'path' => '/actors'],
+            ['title' => $actor->getName(), 'path' => '/actors/'.$args['actorId']],
+        ]);
+        $f3->set('actor', $actor);
+        $f3->set('pageTitle', $actor->getName());
+        $f3->set('content', 'actor.htm');
         echo \Template::instance()->render('main.htm');
     }
 }
